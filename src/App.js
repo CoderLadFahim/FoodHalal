ade149c9f76a5218f33b6c73ea4ef51aa411a124
Fahim <fahimalemroz@gmail.com>
2021-03-04 21:00:47 +0600

    Application set up for development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let p = useRef(null);
+
+	useEffect(() => {
+		gsap.from(p, {
+			duration: 1,
+			opacity: 0,
+			yPercent: -100,
+			ease: 'elastic.out',
+			stagger: 0.1,
+		});
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p className="animate" ref={el => (p = el)}>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
