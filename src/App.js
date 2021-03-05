eaee2dfd1ce503e85896df8033c6ab0c65e9b890
Fahim <fahimalemroz@gmail.com>
2021-03-05 08:23:18 +0600

    Colors declared as sass vars

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ function App() {
 	let p = useRef(null);
 
 	useEffect(() => {
-		gsap.from(p, { scale: 0 });
+		gsap.from(p, {
+			duration: 1,
+			opacity: 0,
+			yPercent: -100,
+			ease: 'elastic.out',
+			stagger: 0.1,
+		});
 	}, []);
 
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p ref={el => (p = el)}>
+				<p className="animate" ref={el => (p = el)}>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a
