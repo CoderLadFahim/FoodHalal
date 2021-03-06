b16d133b9326f982f7ce0f440772ac3577b84b33
Fahim <fahimalemroz@gmail.com>
2021-03-06 16:24:54 +0600

    NavBar added

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+import './NavBar.scss';
+
+function NavBar() {
+	return (
+		<nav>
+			<ul className="wrapper">
+				<li className="home-link logo-grad">
+					<Link to="/">FH</Link>
+				</li>
+				<li>
+					<Link to="/menu">Menu</Link>
+				</li>
+				<li>
+					<Link to="/contact">Contact</Link>
+				</li>
+				<li>
+					<Link to="/about">About</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
+
+export default NavBar;
