843552f60a644855195c6dc4f0043706d51c835d
Fahim <fahimalemroz@gmail.com>
2021-04-06 19:51:40 +0600

    Contact form Functional

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -1,12 +1,25 @@
 import './ContactStyles.scss';
 
 import TweenLite from 'gsap';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import AppInput from '../components/AppInput';
 import PageTitle from '../components/PageTitle';
 
 function Contact(props) {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [subject, setSubject] = useState('');
+	const [userIssue, setUserIssue] = useState('');
+
+	const requiredFieldsFilled = [name, email, subject, userIssue].every(
+		field => field
+	);
+
+	const submitBtnClass = `submit-btn ${
+		!requiredFieldsFilled ? 'disabled' : ''
+	}`;
+
 	useEffect(() => {
 		const contactPageTimeline = TweenLite.timeline();
 
@@ -48,12 +61,26 @@ function Contact(props) {
 		<section className="contact-page wrapper">
 			<PageTitle>Contact</PageTitle>
 
-			<form className="contact-form" onSubmit={e => e.preventDefault()}>
-				<AppInput> Full Name </AppInput>
-				<AppInput> Email / Phone </AppInput>
-				<AppInput> Subject </AppInput>
-				<textarea required placeholder="Tell us your issue"></textarea>
-				<button className="submit-btn">Submit</button>
+			<form className="contact-form">
+				<AppInput content={name} contentSetter={setName}>
+					{' '}
+					Full Name{' '}
+				</AppInput>
+				<AppInput content={email} contentSetter={setEmail}>
+					{' '}
+					Email / Phone{' '}
+				</AppInput>
+				<AppInput content={subject} contentSetter={setSubject}>
+					{' '}
+					Subject{' '}
+				</AppInput>
+				<textarea
+					value={userIssue}
+					onChange={e => setUserIssue(prevIssue => e.target.value)}
+					required
+					placeholder="Tell us your issue"
+				></textarea>
+				<button className={submitBtnClass}>Submit</button>
 			</form>
 		</section>
 	);
