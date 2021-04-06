0fcab5b61925144a003851d28bc2f67f177e6164
Fahim <fahimalemroz@gmail.com>
2021-04-06 19:44:57 +0600

    footer done

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -3,11 +3,8 @@ import './AppFooterStyles.scss';
 export default function AppFooter() {
 	return (
 		<footer>
-			<div className="content">
-				<h1>This site is completely fictional</h1>
-				<p>Made by CoderLadFahim</p>
-			</div>
-			<img src="../assets/small2.png" alt="footer" />
+			<h1>This site is completely fictional</h1>
+			<p>Made by CoderLadFahim</p>
 		</footer>
 	);
 }
