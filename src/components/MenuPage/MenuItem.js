6fb1121a3e215fb2bed0bbee5a6869b95750eca2
Fahim <fahimalemroz@gmail.com>
2021-04-15 15:45:06 +0600

    Menu item added

---
+++ src/components/MenuPage/MenuItem.js
@@ -0,0 +1,15 @@
+function MenuItem(props) {
+	return (
+		<div className="menu-item">
+			<div className="food-image">
+				<span className="price">$10</span>
+			</div>
+			<div className="item-control">
+				<p className="item-name">Chicken Skewers</p>
+				<button className="add-cart-btn">Add</button>
+			</div>
+		</div>
+	);
+}
+
+export default MenuItem;
