5c9203d9c44d59eb584f03c535154bc7082d6bbd
Fahim <fahimalemroz@gmail.com>
2021-04-23 20:25:59 +0600

    foodItems being displayed and added item type toggle functionality

--- src/components/MenuPage/MenuItem.js
+++ src/components/MenuPage/MenuItem.js
@@ -1,23 +1,32 @@
 import './MenuItemStyles.scss';
 import CountControl from '../CountControl';
 
-function MenuItem(props) {
+function MenuItem({ foodItem }) {
 	return (
-		<div className="menu-item">
+		<div
+			className="menu-item"
+			style={{
+				width: 360,
+				height: 300,
+				backgroundImage: `url(${foodItem.img})`,
+			}}
+		>
 			<div className="item-control">
 				<button className="add-remove-btn">Add Item</button>
-				<div className="item-count-control">
-					<CountControl></CountControl>
-					<p className="total-item-price">
-						Total:
-						<span className="total-price"></span>
-					</p>
-				</div>
+				{null && (
+					<div className="item-count-control">
+						<CountControl></CountControl>
+						<p className="total-item-price">
+							Total:
+							<span className="total-price"></span>
+						</p>
+					</div>
+				)}
 			</div>
 
 			<div className="item-label">
-				<p className="item-name">ITEM_NAME</p>
-				<p className="item-price">ITEM_PRICE</p>
+				<p className="item-name">{foodItem.name}</p>
+				<p className="item-price">{foodItem.price}</p>
 			</div>
 		</div>
 	);
--- src/views/Menu.js
+++ src/views/Menu.js
@@ -1,11 +1,12 @@
 import './MenuStyles.scss';
 
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { CartContext } from '../contexts/CartContext';
 import { NavLink } from 'react-router-dom';
-import PageTitle from '../components/PageTitle.js';
+import PageTitle from '../components/PageTitle';
 import menuItems from '../assets/foodItems.json';
+import MenuItem from '../components/MenuPage/MenuItem';
 
 function Menu(props) {
 	const { dispatch } = useContext(CartContext);
@@ -20,12 +21,18 @@ function Menu(props) {
 		};
 	};
 
+	const [activeItemType, setActiveItemType] = useState('appetizers');
+
 	useEffect(() => {
 		// dispatch({
 		// 	type: '<ACTION_TYPE_HERE>',
 		// 	actionPayload: '<ACTION_PAYLOAD_HERE>',
 		// });
 	}, []);
+
+	const itemsToShow = menuItems[activeItemType].map(item => (
+		<MenuItem foodItem={item} key={item.id} />
+	));
 
 	return (
 		<section className="menu-page">
@@ -36,12 +43,20 @@ function Menu(props) {
 				</NavLink>
 			</div>
 
-			<ul className="food-menu">
-				<li className="food-link">Appetisers</li>
+			<ul
+				className="food-menu"
+				onClick={e => {
+					e.target.className === 'food-link' &&
+						setActiveItemType(e.target.textContent.toLowerCase());
+				}}
+			>
+				<li className="food-link">Appetizers</li>
 				<li className="food-link">Main</li>
 				<li className="food-link">Desserts</li>
 				<li className="food-link">Drinks</li>
 			</ul>
+
+			{itemsToShow}
 		</section>
 	);
 }
