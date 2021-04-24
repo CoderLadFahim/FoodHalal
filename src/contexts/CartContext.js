c4d0d8b1fbb04d3bf269d5f651a0e09c6ff79c35
Fahim <fahimalemroz@gmail.com>
2021-04-24 16:11:55 +0600

    unnecessary comments removed

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -5,43 +5,17 @@ export const CartContext = createContext();
 const cartReducer = (state, action) => {
 	switch (action.type) {
 		case 'ADD_ITEM':
-			// incrementing the item count if action.item exists in state
-			const potentialItemExistance = state.find(
-				item => item.id === action.item.id
-			);
-
-			if (potentialItemExistance)
-				return cartReducer(state, {
-					type: 'INCREMENT_ITEM_COUNT',
-					itemID: action.item.id,
-				});
-
-			// adding action.item to state
 			return [...state, action.item];
 		case 'REMOVE_ITEM':
 			return state.filter(item => item.id !== action.itemID);
 		case 'INCREMENT_ITEM_COUNT':
 			const itemToIncrement = state.find(item => item.id === action.itemID);
-			// itemToIncrement.count++;
-
-			// removing the item with old count value and adding the new one with update count value
 			return [
 				...state.filter(item => item.id !== action.itemID),
 				{ ...itemToIncrement, count: itemToIncrement.count + 1 },
 			];
 		case 'DECREMENT_ITEM_COUNT':
-			// replicating the same functionality as the INCREMENT_ITEM_COUNT case  but decrementing
 			const itemToDecrement = state.find(item => item.id === action.itemID);
-
-			//	removing the item if only 1 exists
-			// if (itemToDecrement.count === 1)
-			// return cartReducer(state, {
-			// 	type: 'REMOVE_ITEM',
-			// 	itemID: action.itemID,
-			// });
-
-			// itemToDecrement.count--;
-
 			return [
 				...state.filter(item => item.id !== action.itemID),
 				{
@@ -56,24 +30,24 @@ const cartReducer = (state, action) => {
 
 function CartContextProvider({ children }) {
 	const [cartItems, dispatch] = useReducer(cartReducer, [
-		// {
-		// 	name: 'Caviar Sandwich',
-		// 	id: 'caviar_sandwich',
-		// 	price: '123',
-		// 	count: 1,
-		// },
-		// {
-		// 	name: 'Dumplings',
-		// 	id: 'dumplings',
-		// 	price: '123',
-		// 	count: 1,
-		// },
-		// {
-		// 	name: 'Pumpkin Soup',
-		// 	id: 'pumpkin_soup',
-		// 	price: '123',
-		// 	count: 1,
-		// },
+		{
+			name: 'Caviar Sandwich',
+			id: 'caviar_sandwich',
+			price: '123',
+			count: 1,
+		},
+		{
+			name: 'Dumplings',
+			id: 'dumplings',
+			price: '123',
+			count: 1,
+		},
+		{
+			name: 'Pumpkin Soup',
+			id: 'pumpkin_soup',
+			price: '123',
+			count: 1,
+		},
 	]);
 
 	return (
