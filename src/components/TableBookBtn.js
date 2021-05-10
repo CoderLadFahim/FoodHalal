15a8d8b9696e9853e4e5241575756f8d9eb825e9
Fahim <fahimalemroz@gmail.com>
2021-05-10 21:35:09 +0600

    Using the TableBookBtn on the homeview

--- src/components/TableBookBtn.js
+++ src/components/TableBookBtn.js
@@ -1,16 +1,16 @@
 import './TableBookBtnStyles.scss';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function TableBookBtn() {
 	return (
-		<Link to="/book">
+		<NavLink to="/book">
 			<button className="btn table-book-btn">
 				<span className="btn-text"> BOOK A TABLE </span>
 				<FontAwesomeIcon className="arrow-right" icon={faArrowRight} />
 			</button>
-		</Link>
+		</NavLink>
 	);
 }
 
