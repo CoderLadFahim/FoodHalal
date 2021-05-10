39f33fcc3ca6f4b822c8dded62e8960822a3b6ed
Fahim <fahimalemroz@gmail.com>
2021-05-10 20:58:43 +0600

    Started working on the landing page

--- src/views/Home.js
+++ src/views/Home.js
@@ -11,30 +11,33 @@ import small1 from '../assets/images_global/small1.png';
 function Home(props) {
 	return (
 		<section className="home-page">
-			<h1 className="app-logo">FoodHalal</h1>
-			<p className="app-description">
-				<span className="line1">Enjoy our top-quality</span>
-				<span className="line2">professionally cooked meals</span>
-				<span className="line3">that are 100% Halal certified</span>
-			</p>
-			<div className="app-btns">
-				<Link to="/book">
-					<button className="btn">
-						BOOK A TABLE
-						<FontAwesomeIcon className="arrow-right" icon={faArrowRight} />
-						{/* Arrow here */}
-					</button>
-				</Link>
-
-				<Link to="/menu">
-					<button className="btn btn-secondary">ORDER NOW</button>
-				</Link>
-			</div>
 			<div className="decorations">
 				<img src={picMain} alt="Large meal on white plate" />
 				<img src={bottomLeft} alt="Tacos on serving platter" />
 				<img src={bottomRight} alt="Fresh green salad" />
 				<img src={small1} alt="Fish Grill with Salad" />
+			</div>
+
+			<div className="page-content">
+				<h1 className="app-logo">FoodHalal</h1>
+				<p className="app-description">
+					<span className="line1">Enjoy our top-quality</span>
+					<span className="line2">professionally cooked meals</span>
+					<span className="line3">that are 100% Halal certified</span>
+				</p>
+				<div className="app-btns">
+					<Link to="/book">
+						<button className="btn">
+							BOOK A TABLE
+							<FontAwesomeIcon className="arrow-right" icon={faArrowRight} />
+							{/* Arrow here */}
+						</button>
+					</Link>
+
+					<Link to="/menu">
+						<button className="btn btn-secondary">ORDER NOW</button>
+					</Link>
+				</div>
 			</div>
 		</section>
 	);
