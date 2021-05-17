2d22a16b84f439a00ad198042e64fd4d228bd56c
Fahim <fahimalemroz@gmail.com>
2021-05-17 22:42:32 +0600

    Mobile homepage anim done

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,7 @@
 import './HomeStyles.scss';
 import { Link } from 'react-router-dom';
+import TweenLite from 'gsap';
+import { useEffect } from 'react';
 
 import TableBookBtn from '../components/TableBookBtn';
 
@@ -9,6 +11,56 @@ import bottomRight from '../assets/images_global/bottom-right.png';
 import small1 from '../assets/images_global/small1.png';
 
 function Home(props) {
+	useEffect(() => {
+		const homePageTL = TweenLite.timeline({
+			defaults: { ease: 'back.out' /*delay: 5 */ },
+		});
+
+		if (window.innerWidth < 1280) {
+			// the sole small salad anim (only for mobile)
+			homePageTL.from(
+				'.small-salad',
+				{
+					duration: 1,
+					scale: 0,
+					ease: 'back.out',
+				}
+				// '-=1'
+			);
+		} else {
+			homePageTL.from('.homeNav .route-link', {
+				duration: 0.5,
+				y: 100,
+				rotate: 45,
+				stagger: 0.1,
+				opacity: 0,
+				ease: 'power1.out',
+			});
+		}
+
+		// app-logo anim
+		homePageTL.from('.app-logo', {
+			duration: 1,
+			yPercent: 100,
+			opacity: 0,
+		});
+
+		// the description lines
+		homePageTL.from('.desc-line', {
+			duration: 1,
+			yPercent: 300,
+			opacity: 0,
+			stagger: 0.1,
+		});
+
+		homePageTL.from('.app-btns button', {
+			duration: 0.5,
+			xPercent: -100,
+			opacity: 0,
+			stagger: 0.1,
+		});
+	}, []);
+
 	return (
 		<section className="home-page">
 			<div className="decorations">
@@ -33,9 +85,9 @@ function Home(props) {
 			<div className="page-content wrapper">
 				<h1 className="app-logo logo-grad">FoodHalal</h1>
 				<p className="app-description">
-					<span className="line1">Enjoy our top-quality</span>
-					<span className="line2">professionally cooked meals</span>
-					<span className="line3">that are 100% Halal certified</span>
+					<span className="desc-line line1">Enjoy our top-quality</span>
+					<span className="desc-line line2">professionally cooked meals</span>
+					<span className="desc-line line3">that are 100% Halal certified</span>
 				</p>
 				<div className="app-btns">
 					<TableBookBtn />
